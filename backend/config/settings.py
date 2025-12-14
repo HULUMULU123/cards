bb7fa79b44b11fe523f71d3933f5ce0c3ae86738
Stas <stas.r.d87@gmail.com>
2025-12-14 17:02:06 +1000

    -

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,4 +131,4 @@ JAZZMIN_SETTINGS = {
 # TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', 'telegram-bot-token')
 TELEGRAM_BOT_TOKEN ='8534040237:AAHaSD_aof0rOVfDDQqejxvX286oA0MC7z0'
 TELEGRAM_PAYMENT_PROVIDER_TOKEN = os.environ.get('TELEGRAM_PAYMENT_PROVIDER_TOKEN', 'payment-provider-token')
-TELEGRAM_BOT_NAME = os.environ.get('TELEGRAM_BOT_NAME', 'your_bot_name')
+TELEGRAM_BOT_NAME = os.environ.get('TELEGRAM_BOT_NAME', 'cards129_bot')
