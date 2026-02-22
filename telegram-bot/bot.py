52bdd24b5ca542aa2cacec8895377f1e3f00970c
Stas <stas.r.d87@gmail.com>
2026-02-22 13:59:35 +1000

    -

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -15,10 +15,8 @@ logger = logging.getLogger(__name__)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BOT_USERNAME = os.getenv("BOT_USERNAME", "AlGiftCards_bot")
-FREE_OPEN_URL = os.getenv(
-    "FREE_OPEN_URL",
-    "https://t.me/AlGiftCards_bot?startapp=open_pack",
-)
+FREE_OPEN_URL = "https://t.me/AlGiftCards_bot?startapp=open_pack"
+
 ARTICLE_URL = "https://telegra.ph/Kak-zarabotat-zvezdy-v-igre-Gift-Cards-01-14-3"
 SUPPORT_URL = "https://t.me/AlekseyfrolovDm"
 START_MESSAGE = (
